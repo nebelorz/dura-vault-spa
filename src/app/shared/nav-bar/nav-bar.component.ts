1783b9a79a6a0ba5c5165a807a14d63f0f5979d4
Alex Gassó <alex.gasso@col.vueling.com>
2025-12-09 00:34:39 +0100

    Add nav-bar Tops (w/ route placeholders)

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -9,4 +9,58 @@ import { ModeButtonComponent } from './mode-button/mode-button.component';
   templateUrl: './nav-bar.component.html',
   imports: [CommonModule, MenubarModule, ButtonModule, ModeButtonComponent],
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  public readonly menuItems = signal([
+    {
+      label: 'Tops',
+      icon: 'pi pi-database',
+      items: [
+        {
+          label: 'Experience',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/experience'],
+        },
+        {
+          label: 'Magic',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/magic'],
+        },
+        {
+          label: 'Fist',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/fist'],
+        },
+        {
+          label: 'Club',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/club'],
+        },
+        {
+          label: 'Sword',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/sword'],
+        },
+        {
+          label: 'Axe',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/axe'],
+        },
+        {
+          label: 'Distance',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/distance'],
+        },
+        {
+          label: 'Shielding',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/shielding'],
+        },
+        {
+          label: 'Fishing',
+          icon: 'pi pi-box',
+          routerLink: ['/tops/fishing'],
+        },
+      ],
+    },
+  ]);
+}
