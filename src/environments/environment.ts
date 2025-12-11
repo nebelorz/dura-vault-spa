96fbdb29959532fc6e0b6c8777abca434424e05c
Alex Gassó <alex.gasso@col.vueling.com>
2025-12-11 14:17:44 +0100

    Upload environments with non-sensitive info

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  dura: {
+    baseURL: 'https://classic.dura-online.com',
+  },
+  supabase: {
+    url: 'https://pzkhnhkxiyyrizmglbpf.supabase.co',
+    anonKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6a2huaGt4aXl5cml6bWdsYnBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5MjcwMzYsImV4cCI6MjA4MDUwMzAzNn0.uw1oJaGUYv3uAZXNzJGndX8dY39dfbi6CWQ1cTRISXU',
+  },
+};
