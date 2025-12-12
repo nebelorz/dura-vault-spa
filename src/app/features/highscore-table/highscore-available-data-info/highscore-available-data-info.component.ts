cca673061af7ddb6e70cf0ffd4f21f4944169062
nebelorz <nebelorz@hotmail.com>
2025-12-12 02:19:17 +0100

    Add missing tooltip module on highscore-available-data-info

--- src/app/features/highscore-table/highscore-available-data-info/highscore-available-data-info.component.ts
+++ src/app/features/highscore-table/highscore-available-data-info/highscore-available-data-info.component.ts
@@ -4,11 +4,13 @@ import { DatePipe } from '@angular/common';
 import { MinimalistIconComponent } from '../../../shared/minimalist-icon/minimalist-icon.component';
 import { ScrapeDateRange } from '../../../core/models/metadata.model';
 
+import { TooltipModule } from 'primeng/tooltip';
+
 @Component({
   selector: 'app-highscore-available-data-info',
   templateUrl: './highscore-available-data-info.component.html',
   styleUrls: ['./highscore-available-data-info.component.scss'],
-  imports: [DatePipe, MinimalistIconComponent],
+  imports: [DatePipe, MinimalistIconComponent, TooltipModule],
 })
 export class HighscoreAvailableDataInfoComponent {
   availableDateRange = input<ScrapeDateRange | null>(null);
