1e62c974b54b50aaa4eb369431b1092eaf313af6
nebelorz <nebelorz@hotmail.com>
2025-12-22 03:31:27 +0100

    Improve styles + tooltips

--- src/app/features/player-detail/player-detail-summary/player-detail-summary.component.ts
+++ src/app/features/player-detail/player-detail-summary/player-detail-summary.component.ts
@@ -2,12 +2,13 @@ import { Component, input } from '@angular/core';
 
 import { PlayerHistorySummary } from '../../../core/models/player-history.model';
 import { AbbreviateNumberPipe } from '../../../shared/pipes/abbreviate-number.pipe';
+import { Tooltip } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-player-detail-summary',
   templateUrl: './player-detail-summary.component.html',
   styleUrls: ['./player-detail-summary.component.scss'],
-  imports: [AbbreviateNumberPipe],
+  imports: [AbbreviateNumberPipe, Tooltip],
 })
 export class PlayerDetailSummaryComponent {
   summary = input.required<PlayerHistorySummary>();
