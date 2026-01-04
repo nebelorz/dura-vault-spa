56c48d52d132991ed8af5cddf6892fcfb1e4d463
nebelorz <nebelorz@hotmail.com>
2026-01-05 00:34:55 +0100

    Remove unused model due to rename + update barrel file

--- src/app/core/models/index.ts
+++ src/app/core/models/index.ts
@@ -2,4 +2,4 @@ export * from './common.model';
 export * from './highscore.model';
 export * from './metadata.model';
 export * from './period.model';
-export * from './player-history.model';
+export * from './player-details.model';
