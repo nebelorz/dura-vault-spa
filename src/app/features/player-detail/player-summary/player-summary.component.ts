dae3e31ae07d192e1359872745256d2ecb8b7666
nebelorz <nebelorz@hotmail.com>
2026-01-05 12:20:37 +0100

    Add loading to player-summary

--- src/app/features/player-detail/player-summary/player-summary.component.ts
+++ src/app/features/player-detail/player-summary/player-summary.component.ts
@@ -3,14 +3,21 @@ import { Component, input } from '@angular/core';
 import { PlayerDetailsSummary, HighscoreSection, TimePeriod } from '@core/models';
 import { PlayerTotalGainsSummaryComponent } from './player-total-gains-summary/player-total-gains-summary.component';
 import { PlayerPerformanceSummaryComponent } from './player-performance-summary/player-performance-summary.component';
+import { LoadingStatusComponent, NoDataStatusComponent } from '@shared/components';
 
 @Component({
   selector: 'app-player-summary',
   templateUrl: './player-summary.component.html',
   styleUrls: ['./player-summary.component.scss'],
-  imports: [PlayerTotalGainsSummaryComponent, PlayerPerformanceSummaryComponent],
+  imports: [
+    PlayerTotalGainsSummaryComponent,
+    PlayerPerformanceSummaryComponent,
+    LoadingStatusComponent,
+    NoDataStatusComponent,
+  ],
 })
 export class PlayerSummaryComponent {
+  loading = input<boolean>(false);
   summary = input.required<PlayerDetailsSummary>();
   section = input.required<HighscoreSection>();
   selectedPeriod = input.required<TimePeriod>();
