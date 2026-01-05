3918841aef229d786d81704b081b9e3c39933695
nebelorz <nebelorz@hotmail.com>
2026-01-05 12:38:30 +0100

    Add conditionals to player-details if there's no data

--- src/app/features/player-detail/player-summary/player-summary.component.ts
+++ src/app/features/player-detail/player-summary/player-summary.component.ts
@@ -3,21 +3,14 @@ import { Component, input } from '@angular/core';
 import { PlayerDetailsSummary, HighscoreSection, TimePeriod } from '@core/models';
 import { PlayerTotalGainsSummaryComponent } from './player-total-gains-summary/player-total-gains-summary.component';
 import { PlayerPerformanceSummaryComponent } from './player-performance-summary/player-performance-summary.component';
-import { LoadingStatusComponent, NoDataStatusComponent } from '@shared/components';
 
 @Component({
   selector: 'app-player-summary',
   templateUrl: './player-summary.component.html',
   styleUrls: ['./player-summary.component.scss'],
-  imports: [
-    PlayerTotalGainsSummaryComponent,
-    PlayerPerformanceSummaryComponent,
-    LoadingStatusComponent,
-    NoDataStatusComponent,
-  ],
+  imports: [PlayerTotalGainsSummaryComponent, PlayerPerformanceSummaryComponent],
 })
 export class PlayerSummaryComponent {
-  loading = input<boolean>(false);
   summary = input.required<PlayerDetailsSummary>();
   section = input.required<HighscoreSection>();
   selectedPeriod = input.required<TimePeriod>();
