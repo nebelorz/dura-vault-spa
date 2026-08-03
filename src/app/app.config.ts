1c865cc2ea5379d69faeba74435e43eaf448989e
nebelorz <nebelorzbox@gmail.com>
2026-08-04 00:11:52 +0200

    style(theme): set emerald as primary color
    Added emerald primary to PrimeNG theme options

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,6 +15,7 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura,
         options: {
+          primary: 'emerald',
           darkModeSelector: '.darkmode',
         },
       },
