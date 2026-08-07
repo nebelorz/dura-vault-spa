e19985b85a9b0acabb782421b1c97af0945c3a32
nebelorz <nebelorzbox@gmail.com>
2026-08-07 15:32:41 +0200

    chore: update dev-post v2.5.2

--- src/app/features/landing-page/dev-info-panel/dev-info-panel.component.ts
+++ src/app/features/landing-page/dev-info-panel/dev-info-panel.component.ts
@@ -81,7 +81,7 @@ export class DevInfoPanelComponent implements OnInit {
 
   // Ordered list of post dates (newest first)
   readonly postDates = [
-    '2026-08-06',
+    '2026-08-07',
     '2026-08-02',
     '2026-07-27',
     '2026-06-05',
