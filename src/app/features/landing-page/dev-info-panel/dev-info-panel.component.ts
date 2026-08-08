cc54c85744e2bed73380fdca5b4602e74e79dd47
nebelorz <nebelorzbox@gmail.com>
2026-08-08 17:26:42 +0200

    docs(dev-posts): add 2.5.3 release post

--- src/app/features/landing-page/dev-info-panel/dev-info-panel.component.ts
+++ src/app/features/landing-page/dev-info-panel/dev-info-panel.component.ts
@@ -81,6 +81,7 @@ export class DevInfoPanelComponent implements OnInit {
 
   // Ordered list of post dates (newest first)
   readonly postDates = [
+    '2026-08-08',
     '2026-08-07',
     '2026-08-02',
     '2026-07-27',
